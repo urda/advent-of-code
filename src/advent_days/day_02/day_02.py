004d45b1f36685360fabb7dca580c743d29b6b53
Peter Urda <peter.urda@gmail.com>
2021-12-02 02:42:04 -0800

    Init Day 02

---
+++ src/advent_days/day_02/day_02.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+class Day02:
+    _working_dir = Path(__file__).resolve().parent
+
+    _data_file = 'day_02_data.txt'
+    _data_file_path = Path(_working_dir, _data_file)
