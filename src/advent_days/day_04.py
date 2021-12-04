13db0ae24efd8269cc9cfc65bb0cb4030eab9538
Peter Urda <peter.urda@gmail.com>
2021-12-04 02:40:54 -0800

    Init Day 04

---
+++ src/advent_days/day_04.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from .day_meta import DayMeta
+
+
+class Day04(DayMeta):
+    _data_file = 'day_04.txt'
+
+    @classmethod
+    def solve_day(cls) -> List[str]:
+        pass
