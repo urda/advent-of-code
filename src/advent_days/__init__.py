0d1af8b2ca9cc50bc664fccf2adca15052c32596
Peter Urda <peter.urda@gmail.com>
2021-12-07 18:07:11 -0800

    Init Day 07

--- src/advent_days/__init__.py
+++ src/advent_days/__init__.py
@@ -4,6 +4,7 @@ from .day_03 import Day03
 from .day_04 import Day04
 from .day_05 import Day05
 from .day_06 import Day06
+from .day_07 import Day07
 
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     Day04,
     Day05,
     Day06,
+    Day07,
 ]
---
+++ src/advent_days/day_07.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from .day_meta import DayMeta
+
+
+class Day07(DayMeta):
+    _data_file = 'day_07.txt'
+
+    @classmethod
+    def solve_day(cls) -> List[str]:
+        return [
+        ]
