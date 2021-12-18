32e9a4c023b45bb328a850d200ff19e54f7edffc
Peter Urda <peter.urda@gmail.com>
2021-12-18 14:31:58 -0800

    Fix linting issues (...)
    pylint --output-format=text --rcfile=./.pylintrc ./src/main.py ./src/advent_days ./src/advent_utils
    ************* Module advent_days.day_10
    src/advent_days/day_10.py:1:0: C0114: Missing module docstring (missing-module-docstring)
    src/advent_days/day_10.py:42:4: C0116: Missing function or method docstring (missing-function-docstring)
    src/advent_days/day_10.py:51:4: C0116: Missing function or method docstring (missing-function-docstring)
    src/advent_days/day_10.py:71:16: R1705: Unnecessary "else" after "return" (no-else-return)
    src/advent_days/day_10.py:62:12: W0612: Unused variable 'idx' (unused-variable)
    
    -------------------------------------------------------------------
    Your code has been rated at 9.91/10 (previous run: 10.00/10, -0.09)
    
    make: *** [pylint] Error 28

--- src/advent_days/day_10.py
+++ src/advent_days/day_10.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2021 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from typing import List
 
 from advent_days.day_meta import DayMeta
@@ -40,16 +56,28 @@ class Day10(DayMeta):
 
     @classmethod
     def parse_syntax(cls, nav_data: List[str]) -> int:
-        result = 0
+        """
+        Parse the syntax of the nav data input.
 
+        :param nav_data: The full list of strings of nav data.
+        :return: The "score" of the syntax parse result.
+        """
+
+        result = 0
         for nav_data_entry in nav_data:
             result += cls.parse_syntax_line(nav_data_entry)
-
         return result
 
     @classmethod
-    def parse_syntax_line(cls, nava_data_line: str) -> int:
-        nav_data_chars = [*nava_data_line]
+    def parse_syntax_line(cls, nav_data_line: str) -> int:
+        """
+        Parse the syntax of a single line of nav data input.
+
+        :param nav_data_line: The singular line of nav data.
+        :return: The "score" of the syntax parse result.
+        """
+
+        nav_data_chars = [*nav_data_line]
 
         nav_tracker = {
             cls.open_bracket: 0,
@@ -59,7 +87,7 @@ class Day10(DayMeta):
         }
 
         open_stack = []
-        for idx, character in enumerate(nav_data_chars):
+        for character in nav_data_chars:
             if character not in cls.all_chars:
                 raise ValueError('Unknown character in input')
 
@@ -68,10 +96,10 @@ class Day10(DayMeta):
                 open_stack.append(character)
             else:
                 last_open = open_stack.pop()
-                if character != cls.open_to_close_map[last_open]:
-                    return cls.close_points[character]
-                else:
+                if character == cls.open_to_close_map[last_open]:
                     nav_tracker[last_open] -= 1
+                else:
+                    return cls.close_points[character]
 
         return 0
 
