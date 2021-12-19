120e8d8a8cdcb899532d13d462e94a2d4eb182fd
Peter Urda <peter.urda@gmail.com>
2021-12-19 13:18:22 -0800

    Day 10 Part 2 Done

--- src/advent_days/day_10.py
+++ src/advent_days/day_10.py
@@ -13,10 +13,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from typing import List
+import copy
+from enum import Enum
+from typing import (
+    List,
+    Tuple,
+)
 
 from advent_days.day_meta import DayMeta
+
+
+class ParseMode(Enum):
+    """
+    Enum to control parse mode
+    """
+    CORRUPTED = 'CORRUPTED'
+    INCOMPLETE = 'INCOMPLETE'
 
 
 class Day10(DayMeta):
@@ -50,31 +62,79 @@ class Day10(DayMeta):
         close_paren: 3,
     }
 
+    complete_points = {
+        close_bracket: 2,
+        close_carrot: 4,
+        close_curly: 3,
+        close_paren: 1,
+    }
+
     close_chars = {close_bracket, close_carrot, close_curly, close_paren}
     open_chars = {open_bracket, open_carrot, open_curly, open_paren}
     all_chars = close_chars.union(open_chars)
 
     @classmethod
-    def parse_syntax(cls, nav_data: List[str]) -> int:
+    def compute_incomplete_stack(cls, remaining_stack: List[str]) -> int:
+        """
+        Compute the remaining values needed for the stack, and score it.
+
+        :param remaining_stack: The leftover open stack values.
+        :return: The score of the parse result of the incomplete line.
+        """
+        result = 0
+        internal_stack = copy.deepcopy(remaining_stack)
+
+        while len(internal_stack) > 0:
+            character = internal_stack.pop()
+            result = result * 5
+            result += cls.complete_points[cls.open_to_close_map[character]]
+
+        return result
+
+    @classmethod
+    def parse_syntax(cls, nav_data: List[str], parse_mode: ParseMode) -> int:
         """
         Parse the syntax of the nav data input.
 
         :param nav_data: The full list of strings of nav data.
+        :param parse_mode: The parsing mode to process the nav data in.
         :return: The "score" of the syntax parse result.
         """
 
         result = 0
+        incomplete = []
         for nav_data_entry in nav_data:
-            result += cls.parse_syntax_line(nav_data_entry)
+            corrupted_count, counters = \
+                cls.parse_corrupted_lines(nav_data_entry)
+
+            match parse_mode:
+                case ParseMode.CORRUPTED:
+                    result += corrupted_count
+                case ParseMode.INCOMPLETE:
+                    if corrupted_count == 0:
+                        incomplete.append(counters)
+
+        if parse_mode is ParseMode.INCOMPLETE:
+            results = []
+
+            for entry in incomplete:
+                results.append(cls.compute_incomplete_stack(entry))
+
+            results.sort()
+            result = results[len(results) // 2]
+
         return result
 
     @classmethod
-    def parse_syntax_line(cls, nav_data_line: str) -> int:
+    def parse_corrupted_lines(
+            cls,
+            nav_data_line: str
+    ) -> Tuple[int, List[str]]:
         """
         Parse the syntax of a single line of nav data input.
 
         :param nav_data_line: The singular line of nav data.
-        :return: The "score" of the syntax parse result.
+        :return: A tuple of the corrupted score, and incomplete trackers.
         """
 
         nav_data_chars = [*nav_data_line]
@@ -99,16 +159,20 @@ class Day10(DayMeta):
                 if character == cls.open_to_close_map[last_open]:
                     nav_tracker[last_open] -= 1
                 else:
-                    return cls.close_points[character]
+                    return cls.close_points[character], open_stack
 
-        return 0
+        return 0, open_stack
 
     @classmethod
     def solve_day(cls) -> List[str]:
         nav_data = cls.get_lines_as_list_string(cls._data_file)
-        part_1_result = cls.parse_syntax(nav_data)
+        part_1_result = cls.parse_syntax(nav_data, ParseMode.CORRUPTED)
+        part_2_result = cls.parse_syntax(nav_data, ParseMode.INCOMPLETE)
 
         return [
             'Part 1:',
             f'Determined: {part_1_result}',
+            '---',
+            'Part 2',
+            f'Determined: {part_2_result}',
         ]
--- tests/advent_days/test_day10.py
+++ tests/advent_days/test_day10.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
-from advent_days.day_10 import Day10
+from advent_days.day_10 import (
+    Day10,
+    ParseMode,
+)
 
 
 class TestDay10(TestCase):
@@ -18,7 +21,18 @@ class TestDay10(TestCase):
             '<{([{{}}[<[[[<>{}]]]>[]]',
         ]
 
+    def test_compute_incomplete_counter(self):
+        remaining_stack = ['<', '{', '(', '[']
+        expected = 294
+        actual = Day10.compute_incomplete_stack(remaining_stack)
+        self.assertEqual(expected, actual)
+
     def test_part_1_example(self):
         expected = 26397
-        actual = Day10.parse_syntax(self.init_state)
+        actual = Day10.parse_syntax(self.init_state, ParseMode.CORRUPTED)
+        self.assertEqual(expected, actual)
+
+    def test_part_2_example(self):
+        expected = 288957
+        actual = Day10.parse_syntax(self.init_state, ParseMode.INCOMPLETE)
         self.assertEqual(expected, actual)
