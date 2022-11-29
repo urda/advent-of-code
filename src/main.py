26c8d573c1002b80ddeb0ff7ab8f364951c10b26
Peter Urda <peter.urda@gmail.com>
2022-11-29 01:39:22 -0800

    [RESET 2022] Code Dump

--- src/main.py
+++ src/main.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python3
 """
+Copyright 2021-2022 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 The entry point for the ADVENT OF CODE!
 """
 
-from advent_days import (
-    Day01,
-    Day02,
-    Day03,
-    Day04,
-    Day05,
-    Day06,
-    Day07,
-    Day08,
-    Day09,
-    Day10,
-    Day21,
-)
 from advent_utils.menu_utils import (
     build_menu_lookups,
     print_menu,
 )
-from advent_utils.popo import MenuDayOption
 
 menu_options = [
-    MenuDayOption(1, Day01),
-    MenuDayOption(2, Day02),
-    MenuDayOption(3, Day03),
-    MenuDayOption(4, Day04),
-    MenuDayOption(5, Day05),
-    MenuDayOption(6, Day06),
-    MenuDayOption(7, Day07),
-    MenuDayOption(8, Day08),
-    MenuDayOption(9, Day09),
-    MenuDayOption(10, Day10),
-    MenuDayOption(21, Day21),
 ]
 
 
