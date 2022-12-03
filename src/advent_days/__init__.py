0b86174b47bf351e34af11417b8776cfd1f82ffd
Peter Urda <peter.urda@gmail.com>
2022-12-03 04:41:32 -0800

    [ORIGIN] Drop for later tool demonstration.
    This reverts commit bb00e7700691f7e2d2bbe2657e680e30bdcad626.

--- src/advent_days/__init__.py
+++ src/advent_days/__init__.py
@@ -17,11 +17,9 @@ limitations under the License.
 from .day_01 import Day01
 from .day_02 import Day02
 from .day_03 import Day03
-from .day_04 import Day04
 
 __all__ = [
     'Day01',
     'Day02',
     'Day03',
-    'Day04',
 ]
--- src/main.py
+++ src/main.py
@@ -21,7 +21,6 @@ from advent_days import (
     Day01,
     Day02,
     Day03,
-    Day04,
 )
 from advent_utils.menu_utils import (
     build_menu_lookups,
@@ -33,7 +32,6 @@ menu_options = [
     MenuDayOption(1, Day01),
     MenuDayOption(2, Day02),
     MenuDayOption(3, Day03),
-    MenuDayOption(4, Day04),
 ]
 
 
