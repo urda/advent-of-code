2137e400dc0491be4a0ca52a0759d26114353a37
Peter Urda <peter.urda@gmail.com>
2022-12-03 22:35:31 -0800

    [HPB] Pre-stage Day 05
    [no ci]

--- src/advent_days/day_05/day_05.py
+++ src/advent_days/day_05/day_05.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021-2022 Peter Urda
+Copyright 2022 Peter Urda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,16 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from .day_01 import Day01
-from .day_02 import Day02
-from .day_03 import Day03
-from .day_04 import Day04
-from .day_05 import Day05
+from typing import List
 
-__all__ = [
-    'Day01',
-    'Day02',
-    'Day03',
-    'Day04',
-    'Day05',
-]
+from ..day_meta import DayMeta
+
+
+class Day05(DayMeta):
+    """
+    Advent of Code 2022, Day 05
+    """
+
+    @classmethod
+    def solve_day(cls) -> List[str]:
+        pass
