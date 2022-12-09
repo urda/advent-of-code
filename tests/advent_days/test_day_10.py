b0a877f4328a04168401e915a7b2c826f4e293e6
Peter Urda <peter.urda@gmail.com>
2022-12-09 12:21:25 -0800

    [HPB] Pre-stage Day 10 [no ci]

--- tests/advent_days/test_day_10.py
+++ tests/advent_days/test_day_10.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021-2022 Peter Urda
+Copyright 2022 Peter Urda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,26 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from .day_01 import Day01
-from .day_02 import Day02
-from .day_03 import Day03
-from .day_04 import Day04
-from .day_05 import Day05
-from .day_06 import Day06
-from .day_07 import Day07
-from .day_08 import Day08
-from .day_09 import Day09
-from .day_10 import Day10
+from unittest import TestCase
 
-__all__ = [
-    'Day01',
-    'Day02',
-    'Day03',
-    'Day04',
-    'Day05',
-    'Day06',
-    'Day07',
-    'Day08',
-    'Day09',
-    'Day10',
-]
+from advent_days import Day10
+
+
+class TestDay10(TestCase):
+    data = None
+
+    def test_part_1(self):
+        expected = True
+        actual = False
+        assert expected == actual
+
+    def test_part_2(self):
+        expected = True
+        actual = False
+        assert expected == actual
