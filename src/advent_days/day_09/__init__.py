03416d662452210bd78a7f7149f451a754a641bb
Peter Urda <peter.urda@gmail.com>
2022-12-08 17:32:39 -0800

    [HPB] Pre-stage Day 09 [no ci]

--- src/advent_days/day_09/__init__.py
+++ src/advent_days/day_09/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021-2022 Peter Urda
+Copyright 2022 Peter Urda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,24 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from .day_01 import Day01
-from .day_02 import Day02
-from .day_03 import Day03
-from .day_04 import Day04
-from .day_05 import Day05
-from .day_06 import Day06
-from .day_07 import Day07
-from .day_08 import Day08
 from .day_09 import Day09
 
+
 __all__ = [
-    'Day01',
-    'Day02',
-    'Day03',
-    'Day04',
-    'Day05',
-    'Day06',
-    'Day07',
-    'Day08',
     'Day09',
 ]
