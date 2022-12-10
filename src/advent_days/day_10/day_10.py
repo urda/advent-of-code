1a29b4dd2d3680721469156c0e64b5008fa11bfa
Peter Urda <peter.urda@gmail.com>
2022-12-09 18:30:41 -0800

    [HPB] Pre-stage Day 10 [no ci]

---
+++ src/advent_days/day_10/day_10.py
@@ -0,0 +1,48 @@
+"""
+Copyright 2022 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from typing import List
+
+from ..day_meta import DayMeta
+
+
+class Day10(DayMeta):
+    """
+    Advent of Code 2022, Day 10
+    """
+
+    @classmethod
+    def solve_day(cls) -> List[str]:
+        return [
+        ]
+
+    @classmethod
+    def compute_part_1(cls, data):
+        """
+        Parse the data and compute for Day 10.
+        :param data: The data from Advent of Code.
+        :returns: The result for Part 1.
+        """
+        pass
+
+    @classmethod
+    def compute_part_2(cls, data):
+        """
+        Parse the data and compute for Day 10.
+        :param data: The data from Advent of Code.
+        :returns: The result for Part 2.
+        """
+        pass
