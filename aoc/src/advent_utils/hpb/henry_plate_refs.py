f6008b4dcc807fdd7f5dadd19361f229244d3b86
Peter Urda <peter.urda@gmail.com>
2023-11-06 12:53:22 -0800

    Fix linting

--- aoc/src/advent_utils/hpb/henry_plate_refs.py
+++ aoc/src/advent_utils/hpb/henry_plate_refs.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import datetime
 import os
 
 from advent_utils.copyright_builder import CopyrightBuilder
